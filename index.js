bd08ef22f24ad58dc8de688edad90c00be215040
Tiiso <theotiiso@gmail.com>
2019-08-23 00:34:39 +0200

    Authentication

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys.js');
 const app = express(); // generates app
+//688963742743-us6tnguo3j93js9jtmaa3hrdrrj28asc.apps.googleusercontent.com
+//secret aEI9BdYQ-jkyGhpA_WzUGcuH
+passport.use(
+    new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token',accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile:', profile);
+    }
+  )
+);
 
-app.get('/', (req, res) =>{
-    res.send({hi: 'there'});
-});
+// route handler 
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+    scope: ['profile', 'email']
+    })
+);
+
+// 2nd route handler
+app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google')
+);
 
 //Deployment 
 const PORT = process.env.PORT || 5000;
