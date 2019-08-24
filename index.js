aa3bd74bf7fe95566097461adbd644bf4916d831
Tiiso <theotiiso@gmail.com>
2019-08-25 01:34:32 +0200

    Mongoose Model Classes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
 const app = express(); // generates app
 
-require('./routes/authRoutes');(app);
+require('./routes/authRoutes')(app);
 
 //Deployment 
 const PORT = process.env.PORT || 5000;
