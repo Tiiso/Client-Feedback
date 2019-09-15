ff88bb1bcf66429fb9bccbc6dac722295de888de
Tiiso <theotiiso@gmail.com>
2019-09-16 01:32:46 +0200

    redirection tweak

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'production') {
+    // we are in prod - return the prod set
+    module.exports = require('.prod');
+} else {
+    // we are in development -return dev keys!!!
+    module.exports = require('.dev');
+}
