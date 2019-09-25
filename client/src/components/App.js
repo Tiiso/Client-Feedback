f4a1ef386e5111375ab6a82ff991e59945ae86d2
Tiiso <theotiiso@gmail.com>
2019-09-25 16:23:46 +0200

    React Router Setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
     return (
-        <p>Today started out cold and now it is warming up. Hopefully tomorrow will be a sunny day.</p>
+        <div>
+            <p>Today started out cold and now it is warming up. Hopefully tomorrow will be a sunny day.</p>
+        </div>   
     );
-}
+};
 
   export default App;
